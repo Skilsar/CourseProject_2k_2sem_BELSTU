54f833ce3c5bce99bf1d85e611b9f53f6094257e
Skilsar <skilsar@gmail.com>
2022-05-24 04:25:29 +0300

    Обновление и фикс

--- KursProject/viewModel/regVM.cs
+++ KursProject/viewModel/regVM.cs
@@ -27,6 +27,7 @@ namespace LightBooking.viewModel
                 photo = value;
                 OnPropertyChanged("Photo");
             } }
+        public bool photoTrue = false;
 
         public regVM( Registration _win)
         {
@@ -37,27 +38,97 @@ namespace LightBooking.viewModel
         public ICommand registrButton => new DelegateCommand(regButton);
         public void regButton()
         {
-            if (!Regex.Match(password, @"(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9@#!^&*.]{4,})$").Success)
+            bool error = false;
+            for (int i = 0; i < 8; i++)
             {
-                if (password == password2 && login != null)
+                switch (i)
+                {
+                    case 0:
+                        {
+                            if (Regex.Match(email, @"^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$").Success == false)
+                            {
+                                MessageBox.Show("Email не может начинаться или заканчиваться точкой\nЭлектронная почта не должна содержать пробелов в строке\nEmail не должен содержать специальных символов(<:, *, ecc)\nEmail не может содержать точки в середине почтового адреса перед символом @.\nEmail может содержать двойной домен(«.de.org» или аналогичная редкость).");
+                                error = true;
+                            }
+                            break;
+                        }
+                    case 1:
+                        {
+                            if(Regex.Match(password, @"(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9@#!^&*.]{4,})$").Success == false)
+                            {
+                                MessageBox.Show("Минимальнаяя длина пароля 4 символа.\nПароль должен содержать только английские буквы и по крайней мере одну цифру и одну букву (может содержать @#!^&*.)");
+                                error = true;
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            if (password != password2)
+                            {
+                                MessageBox.Show("Введенные пароли не совпадают");
+                                error = true;
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            if (Regex.Match(phone, @"^(\+375|80|375) \((29|25|44|33|17)\) [0-9]{3}-[0-9]{2}-[0-9]{2}$").Success == false)
+                            {
+                                MessageBox.Show("Неверный формат номера телефона\nВведите номер телефона в формате\n+375 (25|29|33|44|17) 111-11-11\n+375 (25|29|33|44|17) 111-11-11\n80 (25|29|33|44|17) 111-11-11\n");
+                                error = true;
+                            }
+                            break;
+                        }
+                    case 4:
+                        {
+                            if (photoTrue == false)
+                            {
+                                MessageBox.Show("Выберите фото профиля\nДля этого кликните по круглой иконке с изображением.");
+                                error = true;
+                            }
+                            break;
+                        }
+                    case 5:
+                        {
+                            if (Regex.Match(name, @"^([А-я]+|[A-z]+)$").Success == false && Regex.Match(surname, @"^([А-я]+|[A-z]+)$").Success == false)
+                            {
+                                MessageBox.Show("Имя или фамилия введены неверно.\nВ имени или фамилии должны быть только буквы русского или английского алфавита\nПример: Админ или Admin");
+                                error = true;
+                            }
+                            break;
+                        }
+                    case 7:
+                        {
+                            if (Regex.Match(login, @"^[A-Za-z0-9]+([A-Za-z0-9]*|[._-]?[A-Za-z0-9]+)*$").Success == false)
+                            {
+                                MessageBox.Show("Ошибка в Логине\nЛогин должен содержать только буквы анлийского алфавита и цифры, без пробелов и спец. символо.");
+                                error = true;
+                            }
+                            break;
+                        }
+                }
+                if (error == true) break;
+            }
+            if (error == false)
+            {
+                if (password == password2 && login != null && password != null && password != "" && login != "")
                 {
                     Requests.Reg(login, password, name, surname, phone, email, photo);
+                    Authorization auth = new Authorization();
+                    new ToastContentBuilder().AddText("Уведомление").AddText($"Пользователь {name} успешно зарегитрирован!").Show();
+                    auth.Show();
+                    win.Close();
                 }
                 else
                 {
                     new ToastContentBuilder().AddText("Уведомление").AddText("Данные регистрации введены некорректно").Show();
                 }
+
             }
             else
             {
-                new ToastContentBuilder().AddText("Уведомление").AddText("Ошибка! Введен некорректный пароль!").Show();
-                MessageBox.Show("Пароль должен содержать только английские буквы и по крайней мере одну цифру и одну букву (может содержать @#!^&*.)");
+                new ToastContentBuilder().AddText("Уведомление").AddText("Ошибка! Данные регистрации введены некорректно").Show();
             }
-                
-
-            Authorization auth = new Authorization();
-            auth.Show();
-            win.Close();
         }
         public ICommand logButton => new DelegateCommand(LogButton);
         public void LogButton()
@@ -77,6 +148,7 @@ namespace LightBooking.viewModel
             {
                 string selFileName = openFile.FileName;
                 Photo = selFileName;
+                photoTrue = true;
             }
         }
 
