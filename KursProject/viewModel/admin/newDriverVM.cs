2a26c341cfb82772a329fb3e4360ebd38780bd22
Skilsar <skilsar@gmail.com>
2022-05-25 18:26:21 +0300

    Регулярное

--- KursProject/viewModel/admin/newDriverVM.cs
+++ KursProject/viewModel/admin/newDriverVM.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
@@ -51,18 +52,88 @@ namespace LightBooking.viewModel
         public ICommand registrDriver => new DelegateCommand(AddDriver);
         public void AddDriver()
         {
-            if (password == password2 && login != null)
+            bool error = false;
+            for (int i = 0; i < 7; i++)
             {
-                if (Requests.AddDriver(name, surname, login, password, phone, number_car, brand_car, color_car, count_seats, photo))
+                switch (i)
                 {
-                    new ToastContentBuilder().AddText("Уведомление ПУ Администратора").AddText("Водитель добавлен!").Show();
+                    case 0:
+                        {
+                            if (Regex.Match(login, @"^[A-Za-z0-9]+([A-Za-z0-9]*|[._-]?[A-Za-z0-9]+)*$").Success == false)
+                            {
+                                MessageBox.Show("Ошибка в Логине\nЛогин должен содержать только буквы анлийского алфавита и цифры, без пробелов и спец. символо.");
+                                error = true;
+                            }
+                            break;
+                        }
+                    case 1:
+                        {
+                            if (Regex.Match(password, @"(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9@#!^&*.]{4,})$").Success == false)
+                            {
+                                MessageBox.Show("Минимальнаяя длина пароля 4 символа.\nПароль должен содержать только английские буквы и по крайней мере одну цифру и одну букву (может содержать @#!^&*.)");
+                                error = true;
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            if (password != password2)
+                            {
+                                MessageBox.Show("Введенные пароли не совпадают");
+                                error = true;
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            if (Regex.Match(phone, @"^(\+375|80|375) \((29|25|44|33|17)\) [0-9]{3}-[0-9]{2}-[0-9]{2}$").Success == false)
+                            {
+                                MessageBox.Show("Неверный формат номера телефона\nВведите номер телефона в формате\n+375 (25|29|33|44|17) 111-11-11\n+375 (25|29|33|44|17) 111-11-11\n80 (25|29|33|44|17) 111-11-11\n");
+                                error = true;
+                            }
+                            break;
+                        }
+                    case 4:
+                        {
+                            if (photoTrue == false)
+                            {
+                                MessageBox.Show("Выберите фото профиля\nДля этого кликните по круглой иконке с изображением.");
+                                error = true;
+                            }
+                            break;
+                        }
+                    case 5:
+                        {
+                            if (Regex.Match(name, @"^([А-я]+|[A-z]+)$").Success == false && Regex.Match(surname, @"^([А-я]+|[A-z]+)$").Success == false)
+                            {
+                                MessageBox.Show("Имя или фамилия введены неверно.\nВ имени или фамилии должны быть только буквы русского или английского алфавита\nПример: Админ или Admin");
+                                error = true;
+                            }
+                            break;
+                        }
                 }
-                else new ToastContentBuilder().AddText("Уведомление ПУ Администратора").AddText("ОШИБКА! Не удалось добавить водителя!").Show();
+                if (error == true) break;
+            }
 
+            if (error == false)
+            {
+                if (password == password2 && login != null)
+                {
+                    if (Requests.AddDriver(name, surname, login, password, phone, number_car, brand_car, color_car, count_seats, photo))
+                    {
+                        new ToastContentBuilder().AddText("Уведомление ПУ Администратора").AddText("Водитель добавлен!").Show();
+                    }
+                    else new ToastContentBuilder().AddText("Уведомление ПУ Администратора").AddText("ОШИБКА! Не удалось добавить водителя!").Show();
+
+                }
+                else
+                {
+                    new ToastContentBuilder().AddText("Уведомление ПУ Администратора").AddText("Данные введены некорректно").Show();
+                }
             }
             else
             {
-                new ToastContentBuilder().AddText("Уведомление ПУ Администратора").AddText("Данные введены некорректно").Show();
+                new ToastContentBuilder().AddText("Уведомление").AddText("Ошибка! Данные водителя введены некорректно").Show();
             }
         }
     }
